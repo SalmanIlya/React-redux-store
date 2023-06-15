c29a7b91eaf54506a06e730fadebd2e7ce9d55c3
Salamanilyas <salamanilyas101@gmail.com>
2023-06-15 16:17:36 +0500

    add-nav-and-footer

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,14 +1,53 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Home from '../Pages/Home/Home'
+import Store from '../Pages/Product Store/Store'
+import Login from '../Pages/Login&Signup/Login'
+import Register from '../Pages/Login&Signup/Register'
+import About from '../Pages/About/About'
+import Contacts from '../Pages/Contacts/Contacts'
+import Notfound from '../Pages/Notfound'
+import Cart from '../Pages/Card/Cart'
 
 const Routing = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' exect element={<h1>home</h1>} />
-        <Route path='/about' exect element={<h1>about</h1>} />
-        <Route path='/contact' exect element={<h1>contact</h1>} />
-        <Route path='/*' exect element={<h1>not found </h1>} />
+        <Route path='/' exect element={
+        <>
+        <Navbar/>
+        <Home/>
+        <Footer/>
+        </>} />
+        <Route path='/store' exect element={<>
+        <Navbar/>
+        <Store/>
+        <Footer/>
+        </>} />
+        <Route path='/cart' exect element={<>
+        <Navbar/>
+        <Cart/>
+        <Footer/>
+        </>} />
+        <Route path='/login' exect element={
+          <>
+          <Login/>
+          </>
+        } />
+        <Route path='/register' exect element={<Register/>} />
+        <Route path='/about' exect element={<>
+        <Navbar/>
+        <About/>
+        <Footer/>
+        </>} />
+        <Route path='/contact' exect element={<>
+        <Navbar/>
+        <Contacts/>
+        <Footer/>
+        </>} />
+        <Route path='/*' exect element={<Notfound/>} />
 
 
       </Routes>
