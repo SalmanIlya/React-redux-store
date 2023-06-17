b3b27b9db353912c54ed6a6b75f26e5ad39d5790
Salamanilyas <salamanilyas101@gmail.com>
2023-06-17 13:53:20 +0500

    intreagate-redux-with-index

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-
+import Store from './app/Store';
+import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+ <Provider store={Store}> 
 <BrowserRouter>
 <App />
 </BrowserRouter>
+</Provider>
 );
 
 
