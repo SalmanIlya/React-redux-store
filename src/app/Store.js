d0ec2ccf5e87b9d921d06d2b12a78d1272a92923
Salamanilyas <salamanilyas101@gmail.com>
2023-06-20 23:52:43 +0500

    add-in-store-card-and-reducer

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  ProductReducer from "./Porduct";
+import CardReducer from "./CardReducer";
 
 const Store=configureStore({
     reducer:{
-        product:ProductReducer
+        product:ProductReducer,
+        Card:CardReducer
     }
 })
 export default Store
